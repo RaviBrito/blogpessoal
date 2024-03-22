23386b0696ea0240eb52a05f978c78b83cc29d18
Ravi Brito <ravibriton@gmail.com>
2024-03-22 12:47:54 -0300

    Criado testes unitarios

---
+++ src/main/java/com/ravib/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,13 @@
+package com.ravib.blogpessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ravib.blogpessoal.model.Postagem;
+
+
+public interface PostagemRepository extends JpaRepository <Postagem, Long>{
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+}
